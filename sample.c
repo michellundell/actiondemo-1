8faaf2bb3103e10e04f3dc4f949db3c96b636756
Michel Lundell <michel@lundell.net>
2022-11-16 10:45:43 +0100

    Update sample.c

--- sample.c
+++ sample.c
@@ -6,5 +6,5 @@ int main(int argc, char **argv)
   printf("hello\n");
   fx();
   printf("hello again\n");
-  return(0);
+  return( argc - 1 ); /* if no arguments it return 1-1=0, if one argument it returns 2-1=1 */
 }
