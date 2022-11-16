033790db2802121ec045751fb84023008bccf8f7
Michel Lundell <michel@lundell.net>
2022-11-16 10:57:52 +0100

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "fx.h"
 
-int main(int argc, char **argv)
+int sample(int x)
 {
   printf("hello\n");
   fx();
   printf("hello again\n");
-  return( argc - 1 ); /* if no arguments it return 1-1=0, if one argument it returns 2-1=1 */
+  return( x - 1 );
 }
