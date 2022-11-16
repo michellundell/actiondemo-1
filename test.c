78531c990394ed13ee608c4b9703ce1bd55a09a5
Michel Lundell <michel@lundell.net>
2022-11-16 10:56:25 +0100

    Create test.c

---
+++ test.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+#include "sample.h"
+
+int main(int argc, char **argv)
+{
+    int result = 0;
+    
+    // testcase 1
+    result += sample(1);
+    // testcase 2
+    result += sample(2);
+  
+    return( result );
+}
