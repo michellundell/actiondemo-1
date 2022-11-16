49378fee5802caac645cc14023fd935e7eda6893
Michel Lundell <michel@lundell.net>
2022-11-16 11:03:54 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+
+#include<stdio.h>
+#include "sample.h"
+
+int main(int argc, char **argv)
+{
+  printf("Thanks for purchasing this expensive program\n");
+  int ret = sample(1);
+  return ret;
+}
