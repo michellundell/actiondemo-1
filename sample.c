d6760817809d926100c603c45349f9869e157745
Michel Lundell <michel@lundell.net>
2022-11-17 06:04:04 +0100

    Update sample.c

--- sample.c
+++ sample.c
@@ -14,11 +14,13 @@ int copy_buf (char *to, int pos, char *from, int len)
 
 int sample(int x)
 {
+  /* 
   char *tobuf="000000";
   char *frombuf="12345678910";
+  */
   printf("sample(%d)\n",x);
   fx();
-  copy_buf(tobuf,3,frombuf,4);
+  /* copy_buf(tobuf,3,frombuf,4); */
   printf("returning(%d)\n",0);
   return( 0 );
 }
