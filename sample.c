b11e3a73e1e1096cd45a5b6303ad069f45b37ffa
Michel Lundell <michel@lundell.net>
2022-11-17 05:47:34 +0100

    Update sample.c

--- sample.c
+++ sample.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include "fx.h"
 
+int copy_buf (char *to, int pos, char *from, int len)
+{
+    int i;
+    for (i=0;i<len;<i++){
+         to[pos] = from [i];
+         pos++;
+    }
+    return pos
+}
+
 int sample(int x)
 {
-  printf("hello running with x = %d\n",x);
+  char *tobuf="000000";
+  char *frombuf="12345678910";
+  printf("sample(%d)\n",x);
   fx();
+  copy_buf(tobuf,3,frombuf,4);
   printf("returning(%d)\n",0);
   return( 0 );
 }
